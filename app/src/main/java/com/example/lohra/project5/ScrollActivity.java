c634e2dfa4104ec6fc064f009bbcf87f9f97b0f3
lohra <rahul@hatchitup.com>
2016-10-18 18:24:45 +0530

    ss

--- app/src/main/java/com/example/lohra/project5/ScrollActivity.java
+++ app/src/main/java/com/example/lohra/project5/ScrollActivity.java
@@ -1,18 +1,13 @@
 package com.example.lohra.project5;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ScrollActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(this,ScrollActivity.class));
+        setContentView(R.layout.activity_scroll);
     }
-
 }
